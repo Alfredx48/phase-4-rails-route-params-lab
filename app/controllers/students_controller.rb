a4b413c77cc0035786b738c4b12293f7da60e84f
Alfred Shaheen <shaheenalfred48@gmail.com>
2022-12-12 16:42:28 -0800

    commit tree 2def8f31ae1e623a884567053d8eb0586258e014

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,22 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    render json: students, status: :ok
+  end
+
+  def grades
+    students = Student.order(grade: :desc)
+    render json: students, status: :ok
+  end
+
+  def highest_grade
+    student = Student.order(grade: :desc).first
+    render json: student, status: :ok
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student, status: :ok
   end
 
 end
